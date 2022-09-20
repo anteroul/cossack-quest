602d74f1df3d1c23b2bb314ca7b0bf1a18198c67
Uljas Lindell <58420474+ValiantInteractive@users.noreply.github.com>
2022-09-20 17:43:34 +0300

    Delete build directory

--- build/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ida/VSCodeProjects/CPP/Valiant-Engine-V2/build/_deps/raylib-src/src
-# Build directory: /Users/ida/VSCodeProjects/CPP/Valiant-Engine-V2/build/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
