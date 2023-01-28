7c4a787eda2ce52d8f46c182884598c6ca81f733
valiant <antero.lindell@gmail.com>
2023-01-28 12:07:50 +0200

    enemy class prototype

--- Cossack-Quest/Enemy.cpp
+++ Cossack-Quest/Enemy.cpp
@@ -25,28 +25,80 @@ Enemy::~Enemy() = default;
 
 void Enemy::update()
 {
-
+	if (alive)
+	{
+		switch (findPath())
+		{
+			default:
+				break;
+		}
+	}
 }
 
 unsigned Enemy::findPath()
 {
 	int direction = NORTH;
-	int distance[8] = {};
+	int distance = 0;
 	int shortestRoute = -1;
 	BoundingBox box = {{position.x * 8.f - 10.f, 0.f, position.z * 8.f - 10.f}, {position.x * 8.f - 6.f, 4.f, position.z * 8.f - 6.f}};
 
 	for (;;)
 	{
-		if (PlayerControl::wallCollision(position, box))
-		{
-
-		}
+		// TODO
+		break;
 	}
 
 	return direction;
 }
 
-bool Enemy::checkCollisionPlayer(BoundingBox &box)
+int Enemy::heuristic(int x1, int y1, int x2, int y2)
 {
-	return false;
+	return abs(x1 - x2) + abs(y1 - y2);
+}
+
+bool Enemy::isValid(int x, int y, int rows, int cols)
+{
+	return x >= 0 && y >= 0 && x < rows && y < cols;
+}
+
+void Enemy::a_star(std::vector<std::vector<int>>& grid, Node start, Node goal)
+{
+	std::priority_queue<Node> open;
+	std::unordered_map<int, std::unordered_map<int, bool>> closed;
+	std::vector<std::vector<int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+	int rows = grid.size();
+	int cols = grid[0].size();
+
+	start.g = 0;
+	start.h = heuristic(start.x, start.y, goal.x, goal.y);
+	start.f = start.g + start.h;
+	open.push(start);
+
+	while (!open.empty()) {
+		Node curr = open.top();
+		open.pop();
+		closed[curr.x][curr.y] = true;
+
+		if (curr.x == goal.x && curr.y == goal.y) {
+			std::cout << "Found a path with a cost of " << curr.g << std::endl;
+			return;
+		}
+
+		for (const auto& dir : directions) {
+			int x = curr.x + dir[0];
+			int y = curr.y + dir[1];
+			if (isValid(x, y, rows, cols) && grid[x][y] != 1 && !closed[x][y]) {
+				Node neighbor;
+				neighbor.x = x;
+				neighbor.y = y;
+				neighbor.g = curr.g + 1;
+				neighbor.h = heuristic(x, y, goal.x, goal.y);
+				neighbor.f = neighbor.g + neighbor.h;
+				open.push(neighbor);
+			}
+		}
+	}
+
+	std::cout << "No path found\n";
 }
--- Cossack-Quest/Enemy.hpp
+++ Cossack-Quest/Enemy.hpp
@@ -1,6 +1,10 @@
 #ifndef COSSACKQUEST_ENEMY_HPP
 #define COSSACKQUEST_ENEMY_HPP
 
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_map>
 #include "GameObject.hpp"
 #include "PlayerControl.hpp"
 
@@ -9,16 +13,30 @@ class Enemy : public GameObject
 public:
 	Enemy(Vector3 pos, Texture tex, Model mod, BoundingBox* wa);
 	~Enemy();
+	void update();
+
 	Vector3 position{};
 	Texture texture;
 	Model model;
-	void update();
 private:
+
+	struct Node {
+		int x, y;
+		int f, g, h;
+
+		bool operator<(const Node& other) const {
+			return f > other.f;
+		}
+	};
+
+	bool alive;
 	BoundingBox* wallArray;
 	BoundingBox* wallIndex;
-	bool checkCollisionPlayer(BoundingBox& box);
+
+	static int heuristic(int x1, int y1, int x2, int y2);
+	static bool isValid(int x, int y, int rows, int cols);
+	void a_star(std::vector<std::vector<int>>& grid, Node start, Node goal);
 	unsigned findPath();
-	bool alive;
 };
 
 
